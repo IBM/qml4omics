23bdf2d1419f301748e37b841f0426277d54af48
Kahn Rhrissorrakrai <krhriss@us.ibm.com>
2025-05-15 11:23:07 -0400

    pass max_iters arg

--- qml4omics/embeddings/compute_nmf.py
+++ qml4omics/embeddings/compute_nmf.py
@@ -3,6 +3,6 @@
 from sklearn.decomposition import NMF
 
 def compute_nmf(X, n_components=None, max_iter=200):
-    nmf = NMF(n_components=n_components)
+    nmf = NMF(n_components=n_components, max_iter=max_iter)
     nmf.fit(X)
     return nmf
